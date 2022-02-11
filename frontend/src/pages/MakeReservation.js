9fad3234ee0167a8df4ec8b94fe98ec1f0a52eff
Jsfrulle <Jessicamhermansson@gmail.com>
2022-02-11 08:24:09 +0100

    fix

--- frontend/src/pages/MakeReservation.js
+++ frontend/src/pages/MakeReservation.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { PostReservations, GetReservations } from "reducers/reservationList";
-import  user from "../reducers/user";
+
 import {  UpdatehUser } from "reducers/user";
 import hotelDetails from "reducers/hotelDetails";
 import { ShowReservationComponent } from "components/ShowReservationComponent";
@@ -13,9 +13,8 @@ export const MakeReservation = () => {
   const coins = useSelector((store) => store.user.coins);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const total = useSelector((store) => store.hotelDetails.totalPrice);
-const price =  parseInt(coins) - parseInt(total)
+
 console.log( parseInt(coins) - parseInt(total) )
 
 
